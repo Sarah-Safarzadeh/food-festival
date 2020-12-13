22c36cc05b22b60fa72ac60a71ff9ae39df11f4b
Sarah-Safarzadeh <Sarah-Safarzadeh@users.noreply.github.com>
2020-12-13 16:10:10 -0600

    added event function

--- service-worker.js
+++ service-worker.js
@@ -18,5 +18,11 @@ const FILES_TO_CACHE = [
 
 // event listener
 self.addEventListener('install', function (e) {
-
-})
+    self.addEventListener('install', function (e) {
+        e.waitUntil(
+          caches.open(CACHE_NAME).then(function (cache) {
+            console.log('installing cache : ' + CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE)
+          })
+        )
+      })
